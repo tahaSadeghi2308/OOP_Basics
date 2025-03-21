d8831bd4670f61a83874423a2f3e6227b43472bd
seyyed <maralahmadi2308@gmail.com>
2025-03-21 23:27:43 +0330

    create instace of calender in main for test it

---
+++ calender/main.cpp
@@ -0,0 +1,11 @@
+#include "calender.hpp"
+
+int main(int argc , char *argv[]){
+    Calender calender;
+    calender.add_event();
+    calender.add_event();
+    calender.refresh();
+    calender.add_event();
+    calender.add_event();
+    return 0;
+}
